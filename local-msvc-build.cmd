412bdf4d740fd715c05cb69d41a3bb15fc92eb96
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-17 22:28:19 +0800

    - update local-msvc-build.cmd

--- local-msvc-build.cmd
+++ local-msvc-build.cmd
@@ -5,10 +5,8 @@ set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
 
 SET PATH=^
-%DOWNLOADS_DIR%\PortableGit\bin;^
-%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
-%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;^
-%DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;
+%DOWNLOADS_DIR%\PortableGit-2.38.1-64-bit\bin;^
+%DOWNLOADS_DIR%\cmake-3.24.0-windows-x86_64\bin;
 
 @REM set PATH=^
 @REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
@@ -26,7 +24,5 @@ cmake --build . --config Release &&^
 echo done &&^
 cmake --install . &&^
 echo "Successful build"
-pause
-
 
             
