43c76259cb8e2645359a07189317d4c187464626
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-18 21:28:24 +0800

    - update local-msvc-build.cmd

--- local-msvc-build.cmd
+++ local-msvc-build.cmd
@@ -13,13 +13,14 @@ SET PATH=^
 @REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
 @REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
+
 cmake.exe -G "Visual Studio 17 2022" -A x64 ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DBUILD_SHARED_LIBS=OFF ^
 -DDAWN_FETCH_DEPENDENCIES=ON  ^
 -DDAWN_ENABLE_INSTALL=ON  ^
--DENABLE_D3D11=OFF ^
--DENABLE_D3D12=OFF ^
+-DDAWN_ENABLE_D3D11=OFF ^
+-DDAWN_ENABLE_D3D12=OFF ^
 -DCMAKE_INSTALL_PREFIX="cmake-build/installation" -B./cmake-build &&^
 cd cmake-build &&^
 cmake --build . --config Release &&^
